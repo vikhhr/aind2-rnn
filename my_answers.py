e2ba0bd5f46723554cfa6bdf2d29a85076316fbd
Victor Hernandez <vikhhr@gmail.com>
2017-10-14 21:02:23 -0500

    Solving project and ready to be trained on GPU

--- my_answers.py
+++ my_answers.py
@@ -3,7 +3,9 @@ import numpy as np
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import LSTM
+from keras.layers import Activation
 import keras
+import re
 
 
 # TODO: fill out the function below that transforms the input series 
@@ -13,22 +15,37 @@ def window_transform_series(series, window_size):
     X = []
     y = []
 
+    len_minus_window_size = len(series) - window_size
+
+    for i in range(0, len_minus_window_size):
+        j = i + window_size
+        X.append(series[i:j])
+        y.append(series[j])
+
     # reshape each 
     X = np.asarray(X)
     X.shape = (np.shape(X)[0:2])
     y = np.asarray(y)
-    y.shape = (len(y),1)
+    y.shape = (len(y), 1)
 
-    return X,y
+    return X, y
 
 # TODO: build an RNN to perform regression on our time series input/output data
 def build_part1_RNN(window_size):
-    pass
-
+    # layer 1 uses an LSTM module with 5 hidden units (note here the input_shape = (window_size,1))
+    # layer 2 uses a fully connected module with one unit
+    return Sequential([
+        LSTM(5, input_shape = (window_size, 1)),
+        Dense(1)
+    ])
 
 ### TODO: return the text input with only ascii lowercase and the punctuation given below included.
 def cleaned_text(text):
     punctuation = ['!', ',', '.', ':', ';', '?']
+    invalid_characters = "[^a-zA-Z" + ''.join(punctuation) + "]"
+
+    pattern = re.compile(invalid_characters);
+    text = pattern.sub(' ', text)
 
     return text
 
@@ -38,9 +55,23 @@ def window_transform_text(text, window_size, step_size):
     inputs = []
     outputs = []
 
-    return inputs,outputs
+    len_minus_window_size = len(text) - window_size
+
+    for i in range(0, len_minus_window_size, step_size):
+        j = i + window_size
+        inputs.append(text[i:j])
+        outputs.append(text[j])
+
+    return inputs, outputs
 
 # TODO build the required RNN model: 
 # a single LSTM hidden layer with softmax activation, categorical_crossentropy loss 
 def build_part2_RNN(window_size, num_chars):
-    pass
+    # layer 1 should be an LSTM module with 200 hidden units --> note this should have input_shape = (window_size,len(chars)) where len(chars) = number of unique characters in your cleaned text
+    # layer 2 should be a linear module, fully connected, with len(chars) hidden units --> where len(chars) = number of unique characters in your cleaned text
+    # layer 3 should be a softmax activation (since we are solving a multiclass classification)
+    return Sequential([
+        LSTM(200, input_shape = (window_size, num_chars)),
+        Dense(num_chars),
+        Activation('softmax')
+    ])
